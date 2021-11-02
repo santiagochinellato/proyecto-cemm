03d0e04329d90c4792b45b2c470d5d025f69d52f
santiagochinellato <santiagochinellato@gmai.com>
2021-11-02 18:09:54 -0300

    se agrego el navbar

---
+++ main.js
@@ -0,0 +1,25 @@
+// // menu mobile
+// var nav = document.querySelector("nav");
+
+// window.addEventListener("scroll", function () {
+//   if (window.pageYOffset > 500 && window.innerWidth <= 767) {
+//     // nav.classList.add('bg-dark', 'shadow');
+//     document.getElementById("display").style.display = "inline";
+//   } else {
+//     // nav.classList.remove('bg-dark', 'shadow');
+//     document.getElementById("display").style.display = "none";
+//   }
+// });
+// // menu mobile
+// // menu desktop
+// var nav = document.getElementById("nav");
+
+// window.addEventListener("scroll", function () {
+//   if (window.pageYOffset > 500) {
+//     // nav.classList.add('menu-desktop', 'shadow');
+//     document.getElementById("nav").style.backgroundColor = "#FFFFFF";
+//   } else {
+//     // nav.classList.remove('menu-desktop', 'shadow')
+//     document.getElementById("nav").style.backgroundColor = "transparent";
+//   }
+// });
