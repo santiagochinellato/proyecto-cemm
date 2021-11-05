b00e514fc683ff4d00937d12ff9186acae380efd
santiagochinellato <santiagochinellato@gmai.com>
2021-11-05 18:55:54 -0300

    se empezó a trabajar en mobile

--- main.js
+++ main.js
@@ -23,3 +23,22 @@
 //     document.getElementById("nav").style.backgroundColor = "transparent";
 //   }
 // });
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
